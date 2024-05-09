3c791c145127d2f958ae38c4a91d7131336797bb
David Arinze <fivelanes72@gmail.com>
2024-05-09 14:03:46 +0100

    secured the pvpRoutes with JWT authentication

--- routes/pvpRouter.ts
+++ routes/pvpRouter.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import authenticateToken from '../middleware/auth';
+
 const router = express.Router();
 
 import { sendPvpRequest, handlePvpAction } from '../controller/pvpController';
 
-router.post('/api/pvp/request', sendPvpRequest);
+router.post('/api/pvp/request', authenticateToken, sendPvpRequest);
   
-router.post('/api/pvp/action', handlePvpAction);
+router.post('/api/pvp/action', authenticateToken, handlePvpAction);
 
 export default router;
